e28451a6ad9d2677360a1b37e1effa8793f22576
Pavlo Rodoslavov <pasharodoslavov@MacBook-Air-Pavlo.local>
2019-01-14 20:58:54 -0800

    some change

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 8000;
-app.use(express.static(__dirname));
 	// Setup pug
 
 app.set('view engine', 'pug');
